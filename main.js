41f7e0df562d3882eb397fc6f65fbba2f4e3648a
Cidalia-madeira <76179167+Cidalia-madeira@users.noreply.github.com>
2021-02-15 12:07:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 var engines = ["1", "2", "3","4", "5", "6", "7", "8"];
 
 
-new Chart(document.getElementById("velocity"),
+new Chart(document.getElementById('velocity'),
 {"type":"line",
 "data":{"labels":["January","February","March","April","May","June","July"],
 "datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],
