fda3fd9faf41872230a3594ed74cd5f8130576d3
Cidalia-madeira <76179167+Cidalia-madeira@users.noreply.github.com>
2021-02-15 11:56:47 +0100

    chart bijgevoegd

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 // main
 
 var engines = ["1", "2", "3","4", "5", "6", "7", "8"];
+
+
+new Chart(document.getElementById("velocity"),
+{"type":"line",
+"data":{"labels":["January","February","March","April","May","June","July"],
+"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],
+"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});
